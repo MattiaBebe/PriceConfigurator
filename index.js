799d7af613687b5c63bfe4a4a930f5bf2c813970
MattiaBebe <mattia.bertinelli@cypag-group.com>
2022-12-21 09:48:56 +0100

    latest changes

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/configuration/:category?', (req, res) => {
     const request = req.params;
     let arrayCategory;
     let level = 'branch';
-    let tipology, tipologyName, index, subcategory;
+    let tipology, tipologyName, index, subcategory, charatteristics;
     if(req.params.category){
         arrayCategory = request.category.split("@");
     }
@@ -41,6 +41,7 @@ app.get('/configuration/:category?', (req, res) => {
                         level = 'leaf';
                         index = cat.index;
                         subcategory = cat.name;
+                        charatteristics = cat.charatteristics;
                     }
                 }   
                 if(cat.is_page && cat.name === category){
@@ -49,10 +50,10 @@ app.get('/configuration/:category?', (req, res) => {
                 }
             })
         })
-        res.json({level: level, tipology: tipology, subcategory: subcategory, index: index, tipologyName: tipologyName, response: response});
+        res.json({level: level, tipology: tipology, subcategory: subcategory, index: index, tipologyName: tipologyName, response: response, charatteristics: charatteristics});
     }
     else{
-        res.json({level: level, tipology: tipology, subcategory: subcategory, index: index, tipologyName: tipologyName, response: configuration.categories});
+        res.json({level: level, tipology: tipology, subcategory: subcategory, index: index, tipologyName: tipologyName, charatteristics: charatteristics, response: configuration.categories});
     }
 })
 
@@ -74,6 +75,16 @@ app.get('/:tipology/:tipologyIndex/diameters', (req, res) => {
     const {tipology, tipologyIndex} = req.params;
     const diameters = Object.keys(mapping.info[tipology][tipologyIndex].diameters);
     res.json({response: 'ok', tipology: tipology, diameters: diameters})
+})
+
+app.get('/caratteristicsValorization/:tipology/:diameter/:tipologyIndex/:key', (req, res) => {
+    res.set('Access-Control-Allow-Origin','*');
+    const {tipology,diameter, tipologyIndex, key} = req.params;
+    const rawInfo = mapping.rawInfo;
+    const raw = mapping.info[tipology][tipologyIndex].carateristics[key];
+    const column = mapping.info[tipology][tipologyIndex].diameters[diameter];
+    const value = mapping.rawInfo[tipology][raw][column];
+    res.json({response: 'ok', value: value})
 })
 
 app.get('/:tipology/:stroke/:diameter/:tipologyIndex', (req, res) => {
--- xlsxXMap.js
+++ xlsxXMap.js
@@ -3,40 +3,87 @@ const reader = require('xlsx');
 const file = reader.readFile('Listino.xlsx');
 const _ = require('lodash'); 
 
+const excludedPages = [
+    'kit ISO15552',
+    'kit cnomo',
+    'kit CP04',
+    'kit CP95',
+    'kit SERIE E',
+    'kit ISO6432 inox-stainless stee',
+    'kit CP04 inox-stainless steel',
+    'kit CP95 inox-stainless steel',
+    'kit 15552 304-stainless steel',
+    'kit 15552 316-stainless steel',
+    'kit 21287-stainless steel'
+];
+
 let data = [];
 let stroke = [];
 let info = {};
 let rawInfo = [];
+let errors = [];
 
 const isDiameterCell = (data) => {
     return data === 'Diametro (ø)' || data === 'Diametro/Bore (ø)'
 }
 
+const carateristics = [
+    'PASSANTE - THROUGH ROD',
+    'STELO AISI316 - SS316 ROD',
+    'STELO PROLUNGATO - ROD EXTENSION (mm)'
+];
+
+const isThrough = (data) => {
+    return data === 'PASSANTE - THROUGH ROD'
+}
+
+const isAISI = (data) => {
+    return data === 'PASSANTE - THROUGH ROD'
+}
+
+const isCharateristic = (data) => {
+    return isThrough(data) || isAISI(data) 
+}
+
 module.exports = () => {
     const sheets = file.SheetNames;
     console.log(file.SheetNames);
+    sheets.filter((s) => !excludedPages.includes(s))
     sheets.map((s) => {
         const rows = reader.utils.sheet_to_json(file.Sheets[s]);
-        let diameterIndex = 0;
+        let tipologyIndex = 0;
+        let through = 0;
         let strokeIndex = -1;
         rawInfo[s] = rows;
         info[s] = {};
         const strokes = {};
         Object.values(rows).map((row, i) => {
             if(isDiameterCell(row.__EMPTY)){
-                info[s][diameterIndex] = {};    
-                diameterIndex++;
+                info[s][tipologyIndex] = {}; 
+                info[s][tipologyIndex].carateristics = {}
+                tipologyIndex++;
             }
+            // if(isThrough(row.__EMPTY)){
+            //     info[s][through] = {};
+            //     through++;
+            // }
         })
-        diameterIndex = 0;
+        tipologyIndex = 0;
+        through = 0;
         Object.values(rows).map((row, i) => { 
             if(isDiameterCell(row.__EMPTY)){
                 const diameters = {};
                 Object.entries(row).map((d) => {
                     diameters[`${d[1]}`] = d[0];
                 });
-                info[s][diameterIndex].diameters = diameters;
-                diameterIndex++;
+                info[s][tipologyIndex].diameters = diameters;
+                tipologyIndex++;
+            }
+            else if(carateristics.includes(row.__EMPTY)){
+                try{
+                    info[s][tipologyIndex-1].carateristics[row.__EMPTY] = i;
+                }
+                catch(e){errors.push(s)}
             }
             else if(row.__EMPTY === 'Corsa/Stroke (mm)'){
                 strokeIndex++;
@@ -48,7 +95,6 @@ module.exports = () => {
                 }
             }
             else if(_.isNumber(row.__EMPTY)){
-                // strokes[`${row.__EMPTY}`] = i;
                 try{
                     info[s][strokeIndex].strokes[`${row.__EMPTY}`] = i;
                 }
@@ -57,12 +103,7 @@ module.exports = () => {
                 };
             }
         })
-        // if(info[s][strokeIndex] ){
-        //     info[s][strokeIndex].strokes = strokes;
-        // }
-        // else{
-        //     info[s].strokes = strokes;
-        // }
+        console.log(errors)
     })
     // const exampleColumn = info.ISO6432.diameters['10'];
     // const exampleRaw = info.ISO6432.strokes['100'];
